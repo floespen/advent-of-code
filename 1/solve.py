af7e3788905a59d070db0a422ae5c2cbc6b5c912
Florian Espenschied <florian.epenschied@gmail.com>
2022-12-15 23:39:30 +0100

    refactor solution to puzzle 1

--- 1/solve.py
+++ 1/solve.py
@@ -1,15 +1,8 @@
 file = open('data', 'r')
 lines = file.readlines()
-  
-count = 0
-for line in lines:
-    line = line.strip()
-    if line=="":
-        count += 1
+
 sumArray = []
 sumArray.append(0)
-print(count)
-
 count = 0
 for line in lines:
     line = line.strip()
